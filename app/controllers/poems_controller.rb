4b53830039cee73d6e86cb8fcf3f105c5a9d8cbc
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-06 12:29:34 +0900

    add poems

---
+++ app/controllers/poems_controller.rb
@@ -0,0 +1,5 @@
+class PoemsController < ApplicationController
+  def index
+    @poems = Poem.all
+    render json: @poems
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs, only: [:index]
-
-  root "blogs#index"
+  resources :poems, only: [:index]
+  
+  #root "blogs#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
